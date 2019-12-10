92ce83528418e1f8d397f65fc4dd891f4776895e
Ritik Jain <ritikjain1002@gmail.com>
2019-12-10 12:59:15 +0530

    update

--- Bear and Species.cpp
+++ Bear and Species.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int bearSpecies(int);
+void _bearSpecies(char**, int**, int, int&);
+bool findPosition(char**, int**, int&, int&, int);
+bool isSafe(int, int, int, char**, int**, int);
 
 int main(void){
     int t, n;
@@ -15,16 +18,102 @@ int main(void){
 }
 
 int bearSpecies(int n){
-    int **matrix = new int*[n];
-    for(int i = 0; i < n; i++)
-        matrix[i] = new int[n];
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            cin >> matrix[i][j];
-     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++)
-            cout << matrix[i][j] << " ";
-        cout << endl;
-     }
+    int res = 0;
+    int flag = 0;
+    char **arr = new char*[n];
+    int **sol = new int*[n];
 
+    for(int i = 0; i < n; i++){
+        arr[i] = new char[n];
+        sol[i] = new int[n];
+    }
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
+            sol[i][j] = 0;
+        }
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            if(arr[i][j] == '?')
+                flag = 1;
+    if(flag == 0)
+        return 0;
+    _bearSpecies(arr, sol, n, res);
+    return res;
+}
+
+bool findPosition(char **arr, int **sol, int &row, int &col, int n){
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            if(arr[i][j] == '?' && sol[i][j] == 0){
+                row = i;
+                col = j;
+                return true;
+            }
+    return false;
+}
+
+void _bearSpecies(char **arr, int **sol, int n, int &res){
+    int row, col;
+    if(!findPosition(arr, sol, row, col, n)){
+        res = (res + 1) % 1000000007;
+        return;
+    }
+    for(int i = 1; i <= 3; i++){
+        if(isSafe(i, row, col, arr, sol, n)){
+            sol[row][col] = i;
+            _bearSpecies(arr, sol, n, res);
+            sol[row][col] = 0;
+        }
+    }
+}
+
+bool isSafe(int i, int row, int col, char **arr, int **sol, int n){
+    int xMove[] = {-1, 0, 1, 0};
+    int yMove[] = {0, 1, 0, -1};
+    if(i == 1){
+        for(int k = 0; k < 4; k++){
+            int _x = row + xMove[k];
+            int _y = col + yMove[k];
+            if(0 <= _x && _x < n && 0 <= _y && _y < n){
+                if(arr[_x][_y] != '.')
+                    return false;
+            }
+        }
+        return true;
+    }
+    else{
+        if(i == 2){
+            for(int k = 0; k < 4; k++){
+                int _x = row + xMove[k];
+                int _y = col + yMove[k];
+                if(0 <= _x && _x < n && 0 <= _y && _y < n){
+                    if(arr[_x][_y] == 'B' || arr[_x][_y] == '.' || arr[_x][_y] == '?'){
+                        if(arr[_x][_y] == '?' && (sol[_x][_y] == 1 || sol[_x][_y] == 3))
+                            return false;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        if(i == 3){
+            for(int k = 0; k < 4; k++){
+                int _x = row + xMove[k];
+                int _y = col + yMove[k];
+                if(0 <= _x && _x < n && 0 <= _y && _y < n){
+                    if(arr[_x][_y] == 'P' || arr[_x][_y] == '.' || arr[_x][_y] == '?'){
+                        if(arr[_x][_y] == '?'  && (sol[_x][_y] == 1 || sol[_x][_y] == 2))
+                            return false;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
 }
