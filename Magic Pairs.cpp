a0af0e65e5f9cfcd752ab22cbbd4573d6ee23ce7
Ritik Jain <ritikjain1002@gmail.com>
2019-12-12 16:09:51 +0530

    Update Magic Pairs.cpp

--- Magic Pairs.cpp
+++ Magic Pairs.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+#define ll unsigned long long
+using namespace std;
 
+int main(void){
+    ll t, n;
+
+    cin >> t;
+    while(t--){
+        cin >> n;
+        ll arr[n];
+        for(ll i = 0; i < n; i++)
+            cin >> arr[i];
+        n--;
+        ll s = n * (n + 1) / 2;
+        cout << s << endl;
+    }
+    return 0;
+}
